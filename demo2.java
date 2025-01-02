5ff75831c94a27dc36294173778a9664d6e4d998
sowmya172 <sowmyasudhagani5@gmail.com>
2025-01-02 14:22:42 +0530

    Add files via upload
    greatest of two

---
+++ demo2.java
@@ -0,0 +1,14 @@
+class demo2{
+    public static void main(String[] args) {
+        int num1,num2;
+       num1=10;
+       num2=20;
+       if(num1>num2) {
+        System.out.println(" num1 is greatest number");
+        
+       }
+       else{
+          System.out.println("num2 is greatest number");
+       }
+        }
+    }
