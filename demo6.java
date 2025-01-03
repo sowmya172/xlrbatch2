a2288478fc8df81bcf97697e17a9aa1f565a6b3b
sowmya172 <sowmyasudhagani5@gmail.com>
2025-01-03 10:09:18 +0530

    Add files via upload

---
+++ demo6.java
@@ -0,0 +1,10 @@
+public class demo6 {
+    public static void main(String[] args) {
+        String username = "Sowmya";
+        String pwd = "Sowmya_22";
+        while((username == "Sowmya") && (pwd == "Sowmya_22")) {
+            System.out.println("hello Sowmya");
+            break;
+        }
+    }
+}
